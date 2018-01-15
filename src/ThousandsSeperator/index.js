8771cc56d004a392e8b0e62fd013e7fdbd178d43
marcosun <marcoinbristol@gmail.com>
2018-01-15 18:03:45 +0800

    accept number and string parameter only, return undefined otherwise

--- src/ThousandsSeperator/index.js
+++ src/ThousandsSeperator/index.js
@@ -1,12 +1,24 @@
 /**
- * [thousandsSeperator thousands seperator]
- * @param  {string/number} data
+ * @module ThousandsSeperator
+ * @param  {string | number} input
  * @return {string}
  */
-export default function thousandsSeperator(data) {
-  if (typeof data === 'boolean' || typeof data === 'object' || isNaN(+data)) {
-    throw data;
-  }
+export default function thousandsSeperator(input) {
+  // Accept Number and String only
+  if (typeof input !== 'number' && typeof input !== 'string') return undefined;
 
-  return data.toString().replace(/^\d+/g, (m) => m.replace(/(?=(?!^)(\d{3})+$)/g, ','));
+  if (isNaN(+input)) return undefined; // NaN cannot be processed
+
+  // const stringify = input.toString();
+
+  // // Integers need to be seperated with dot
+  // // Decimals remain unchanged
+  // const [integers, decimals] = stringify.split('.');
+
+  // //Seperate numbers with comma
+  // const commaIntegers = integers.replace(/\d{1,3}(?=(\d{3})+$)/g, '$&,');
+
+  // return `${commaIntegers}${decimals === void 0 ? '' : '.' + decimals}`;
+
+  return input.toString().replace(/^\d+/g, (m) => m.replace(/(?=(?!^)(\d{3})+$)/g, ','));
 }
