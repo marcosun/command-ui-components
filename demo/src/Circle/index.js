56fcc5bcc84f496284468c34b066f4bd1643b230
marcosun <marcoinbristol@gmail.com>
2018-09-14 09:27:40 +0800

    [demo] Create demo for Circle

--- demo/src/Circle/index.js
+++ demo/src/Circle/index.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import { hot } from 'react-hot-loader';
+import { Circle as IbusCircle } from 'command-ui-components';
 
-export default function Circle() {
-  return <div>Circle</div>;
+export default @hot(module) class Circle extends React.PureComponent {
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          <IbusCircle />
+        </div>
+        <div>
+          <IbusCircle
+            animateDuration={3000}
+            backgroundColor="blue"
+            color={{ 0: 'white', 50: 'yellow', 100: 'red' }}
+            from={25}
+            lineCap="round"
+            lineType={[5, 15]}
+            lineWidth={10}
+            size={200}
+            to={75}
+          />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
